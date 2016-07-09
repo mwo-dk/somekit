501b080a4fb84ea596c5f36b842fdf13f693919e
jimmy <jimmy@baguit.com>
2016-07-09 23:15:37 +0200

    Add mediaType and AuthenticationHeaderValue to HttpClientFactory

--- Source/SomeKit.REST/DotNet Core/SomeKit.REST/HttpClientFactory.cs
+++ Source/SomeKit.REST/DotNet Core/SomeKit.REST/HttpClientFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace SomeKit.REST
 {
@@ -7,10 +8,34 @@ namespace SomeKit.REST
     /// </summary>
     public class HttpClientFactory : IHttpClientFactory
     {
+        private readonly AuthenticationHeaderValue _authenticationHeader;
+        private readonly MediaTypeWithQualityHeaderValue _mediaTypeHeaderValue;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpClientFactory"/> class.
+        /// </summary>
+        /// <param name="authenticationHeader">The authentication header.</param>
+        /// <param name="mediaTypeHeaderValue">The media type header value.</param>
+        public HttpClientFactory(AuthenticationHeaderValue authenticationHeader=null,
+            MediaTypeWithQualityHeaderValue mediaTypeHeaderValue =null)
+        {
+            _authenticationHeader = authenticationHeader;
+            _mediaTypeHeaderValue = mediaTypeHeaderValue;
+        }
+
         /// <inheritdoc />
         public HttpClient Create()
         {
-            return new HttpClient();
+            var client = new HttpClient();
+            if (_authenticationHeader != null)
+            {
+                client.DefaultRequestHeaders.Authorization = _authenticationHeader;
+            }
+            if (_mediaTypeHeaderValue != null)
+            {
+                client.DefaultRequestHeaders.Accept.Add(_mediaTypeHeaderValue);
+            }
+            return client;
         }
     }
 }
