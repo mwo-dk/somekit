d5f499dc46aaf03c2c43130087359b4e3c953142
mwo-dk <michael.w.olesen@gmail.com>
2016-07-10 00:07:35 +0200

    Added basic authentication helper

---
+++ Source/SomeKit.REST/DotNet Core/SomeKit.REST/Extensions/RestClientExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeKit.REST.Extensions
+{
+    public static class RestClientExtensions
+    {
+        public static IRestClient WithBasicAuthentication(this IRestClient client,
+            string user,
+            string password)
+        {
+            if (client == null)
+                throw new ArgumentNullException(nameof(client));
+            if (string.IsNullOrEmpty(user))
+                throw new ArgumentNullException(nameof(user));
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password));
+
+            client.RequestHeaders.Add(new HttpRequestHeader
+            {
+                Name = "Authorization",
+                Value = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{user}:{password}"))
+            });
+
+            return client;
+        }
+    }
+}
